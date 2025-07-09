7136c5ef972b0d88b31c74a03522230cf5d77c0c
Lyne <210183111+Lyne6666@users.noreply.github.com>
2025-07-10 01:44:05 +0700

    Add cmd/autotestnetaikitnext/main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/autotestnetaikitnext/main.go
@@ -0,0 +1,20 @@
+// cmd/autotestnetaikitnext/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	
+	"autotestnetaikitnext/internal/autotestnetaikitnext"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	flag.Parse()
+	
+	app := autotestnetaikitnext.NewApp(*verbose)
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
